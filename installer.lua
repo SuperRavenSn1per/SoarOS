c6d7a5aa15558727fb7eb77ba5b8ba19dd7f60d4
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2022-08-11 14:46:02 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,2 +1,12 @@
+local w,h = term.getSize()
+
+local function download(fileName)
+    local content = http.get(repo..branch..fileName).readAll()
+    local f = fs.open(fileName, "w")
+    f.write(content)
+    f.close()
+end
+
 term.clear()
 term.setCursorPos(1,1)
+

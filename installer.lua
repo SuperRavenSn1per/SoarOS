0c67b3a0b4a368c391528745f1a17dc3269668bf
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2022-08-11 14:49:43 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -7,6 +7,9 @@ local function download(fileName)
     f.close()
 end
 
+term.setBackgroundColor(colors.lightBlue)
 term.clear()
 term.setCursorPos(1,1)
+paintutils.drawLine(1,1,w,1,colors.blue)
+paintutils.drawFilledBox(2,3,w-1,h-1,colors.blue)
 

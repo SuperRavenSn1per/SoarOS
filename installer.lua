963d401bb6b75506c1cc947ce42830d3ca8d1686
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2022-08-11 14:53:14 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -12,4 +12,6 @@ term.clear()
 term.setCursorPos(1,1)
 paintutils.drawLine(1,1,w,1,colors.blue)
 paintutils.drawFilledBox(2,3,w-1,h-1,colors.blue)
+term.setCursorPos(w / 2 - string.len("SoarOS Installer") / 2, 1)
+write("SoarOS Installer")
 
